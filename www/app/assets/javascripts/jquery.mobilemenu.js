9fc623af8ce0556b270578799349bec19b0f79f9
Roy Wei <roywjy@gmail.com>
2016-05-23 15:48:35 +0800

    navbar language switch for mobile version fixed

--- www/app/assets/javascripts/jquery.mobilemenu.js
+++ www/app/assets/javascripts/jquery.mobilemenu.js
@@ -1,17 +1,17 @@
 /**
- * jQuery Mobile Menu 
+ * jQuery Mobile Menu
  * Turn unordered list menu into dropdown select menu
  * version 1.1(27-JULY-2013)
- * 
+ *
  * Built on top of the jQuery library
  *   http://jquery.com
- * 
+ *
  * Documentation
  *   http://github.com/mambows/mobilemenu
  */
 (function($){
 $.fn.mobileMenu = function(options) {
-  
+
   var defaults = {
       defaultText: 'Navigate to...',
       className: 'select-menu',
@@ -20,7 +20,7 @@ $.fn.mobileMenu = function(options) {
     },
     settings = $.extend( defaults, options ),
     el = $(this);
-  
+
   this.each(function(){
     var $el = $(this),
       $select_menu;
@@ -46,10 +46,10 @@ $.fn.mobileMenu = function(options) {
         optSub  = $this.parents( '.' + settings.subMenuClass ),
         len   = optSub.length,
         dash;
-      
+
       // if menu has sub menu
       if( $this.parents('ul').hasClass( settings.subMenuClass ) ) {
-        dash = Array( len+1 ).join( settings.subMenuDash );
+        dash = Array( len + 1 ).join( settings.subMenuDash );
         optText = dash + optText;
       }
 
